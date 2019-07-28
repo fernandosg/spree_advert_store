2bd96f26879a0666b37dc3763a35a02e17864005
Fernando Segura <fernando.segura@bigo.mx>
2019-07-28 15:53:29 -0500

    Se agregan nuevas pruebas para validar la logica del nuevo concern

--- spec/models/advertisment_spec.rb
+++ spec/models/advertisment_spec.rb
@@ -14,4 +14,29 @@ RSpec.describe "Spree::Advertisment", :type => :model do
       expect(advertisment.save).to be_falsy
     end
   end
+
+  context "Logic for advertisments by product model" do
+    let(:advertisment_product) {create(:spree_advertisment, :by_product)}
+
+    it "successfully return a records of advertisments for product" do
+      expect(advertisment_product.advrt_rf.get_advertise(advertisment_product.code)).not_to be_empty
+    end
+
+    it "successfully return an empty records of advertisments for product" do
+      product = create(:product)
+      expect(product.get_advertise(advertisment_product.code)).to be_empty
+    end
+  end
+
+  context "Logic for advertisments by taxon model" do
+    let(:advertisment_taxon) {create(:spree_advertisment, :by_taxon)}
+    it "successfully return a records of advertisments for taxon" do
+      expect(advertisment_taxon.advrt_rf.get_advertise(advertisment_taxon.code)).not_to be_empty
+    end
+
+    it "successfully return an empty records of advertisments for taxon" do
+      taxon = create(:taxon)
+      expect(taxon.get_advertise(advertisment_taxon.code)).to be_empty
+    end
+  end
 end
