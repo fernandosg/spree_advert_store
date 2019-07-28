94068aba716cbb60651bc16d5a916ad2210d619a
Fernando Segura <fernando.segura@bigo.mx>
2019-07-28 16:23:35 -0500

    Se sobreescribe el método method_missing para crear métodos dinámicos que involucren preguntar el tipo de advertisment

--- app/models/spree/advertisment.rb
+++ app/models/spree/advertisment.rb
@@ -1,4 +1,20 @@
 class Spree::Advertisment < ApplicationRecord
   belongs_to :advrt_rf, polymorphic: true
   validates_presence_of :code, :content
+  TYPES_ADVERTISMENT = ["image?", "video?", "text?"]
+
+  def method_helper_is_knowledge(method_name)
+    TYPES_ADVERTISMENT.each do |type|
+      if method_name.include?(type)
+        return true
+      end
+    end
+    return false
+  end
+
+  def method_missing(m, *args, &block)
+    return super(m, *args, &block) unless method_helper_is_knowledge(m.to_s)
+    type = m.to_s.split("_")[-1]
+    return TYPES_ADVERTISMENT.include?(type)
+  end
 end
