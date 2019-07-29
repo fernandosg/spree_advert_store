666fc38ba27cca10a87a8b634c9313a11e4e170d
Fernando Segura <fernando.segura@bigo.mx>
2019-07-28 20:31:08 -0500

    Prueba para validar los métodos dinámicos para obtener si es una imagen/texto/video en el modelo Advertisment

--- spec/models/advertisment_spec.rb
+++ spec/models/advertisment_spec.rb
@@ -39,4 +39,32 @@ RSpec.describe "Spree::Advertisment", :type => :model do
       expect(taxon.get_advertise(advertisment_taxon.code)).to be_empty
     end
   end
+
+  context "type of advertisment" do
+    let(:advertisment) {create(:spree_advertisment, :by_product)}
+
+    it "successfully if calling like image" do
+      advrt_image = create(:spree_advertisment, :by_product, :type_image)
+      expect(advrt_image.is_an_image?).to be_truthy
+    end
+
+    it "successfully if calling like text" do
+      advrt_text = create(:spree_advertisment, :by_product, :type_text)
+      expect(advrt_text.is_a_text?).to be_truthy
+    end
+
+    it "successfully if calling like video" do
+      advrt_video = create(:spree_advertisment, :by_product, :type_video)
+      expect(advrt_video.is_a_video?).to be_truthy
+    end
+
+    it "failure if calling like other type of advertisment" do
+      advrt_video = create(:spree_advertisment, :by_product, :type_video)
+      begin
+        advrt_video.is_a_testing?
+      rescue StandardError => e
+        expect(e).to be_kind_of(NoMethodError)
+      end
+    end
+  end
 end
