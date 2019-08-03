959aba7a8d0d24f8be00e0bf6c7f739f3a4d2aa2
Fernando Segura <fernando.segura@bigo.mx>
2019-08-03 18:12:15 -0500

    Ajuste en la migracion para que el atributo preferences sea de tipo string y que no se tenga inconveniente en la migracion a sqlite

--- db/migrate/20190803214437_create_spree_advertisments_configurations.rb
+++ db/migrate/20190803214437_create_spree_advertisments_configurations.rb
@@ -1,7 +1,7 @@
 class CreateSpreeAdvertismentsConfigurations < ActiveRecord::Migration[5.2]
   def change
     create_table :spree_advertisments_configurations do |t|
-      t.json :preferences
+      t.string :preferences, default: ""
 
       t.timestamps
     end
